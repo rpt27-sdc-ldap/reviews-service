95d8d8a0e2a885c5f650a5b00fc35cf46c816114
Jacob Metevia <63661346+jametevia@users.noreply.github.com>
2021-04-23 12:44:33 -0700

    Add files via upload

---
+++ seedDB.js
@@ -0,0 +1,42 @@
+const Review = require ('./database.js');
+const Faker = require ('faker');
+
+
+const dbObject = {};
+
+for (let i = 0; i < 100; i++) {
+  const reviewCount = Math.floor(50 * Math.random());
+  for (j = 0; j < reviewCount; j++) {
+    const reviewObject = {};
+    reviewObject.bookId = i;
+    const reviewerId = Math.floor(100 * Math.random());
+    reviewObject.reviewerId = reviewerId;
+    const name = Faker.name.findName();
+    const imageUrl = Faker.image.animals();
+    if (dbObject[reviewerId] === undefined) {
+      reviewObject.reviewerName = name;
+      dbObject[reviewerId] = name;
+    } else {
+      reviewObject.name = dbObject[reviewerId];
+    }
+    const urlString = 'imageUrl' + reviewerId.toString();
+    console.log(urlString);
+    if (dbObject[urlString] === undefined) {
+      dbObject[urlString] = imageUrl;
+      reviewObject.urlString = imageUrl;
+    } else {
+      reviewObject.urlString = dbObject[urlString];
+    }
+    const overallStars = Math.floor(Math.random() * 5) + 1;
+    const storyStars = Math.floor(Math.random() * 5) + 1;
+    const performanceStars = Math.floor(Math.random() * 5) + 1;
+    
+    reviewObject.overallStars = overallStars;
+    reviewObject.storyStars = storyStars;
+    reviewObject.performanceStars = performanceStars;
+
+  }
+}
+
+
+
