f30ab3dce241fc6a824cfd9f6a2c8107a8f937f3
Jacob Metevia <ja.metevia@gmail.com>
2021-04-23 13:50:39 -0700

    Working date with correct formatting

--- seedDB.js
+++ seedDB.js
@@ -1,8 +1,13 @@
 const Review = require ('./database.js');
 const Faker = require ('faker');
+const moment = require('moment');
 
 
 const dbObject = {};
+function randomDate(start, end) {
+  return new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime()));
+}
+
 
 for (let i = 0; i < 100; i++) {
   const reviewCount = Math.floor(50 * Math.random());
@@ -33,9 +38,14 @@ for (let i = 0; i < 100; i++) {
     reviewObject.overallStars = overallStars;
     reviewObject.storyStars = storyStars;
     reviewObject.performanceStars = performanceStars;
-    console.log(reviewObject)
+
+    let date = randomDate(new Date(2015, 0, 1), new Date());
+    date = moment(date).calendar();
+    let dateArray = date.split('/');
+    date = dateArray.join('-');
+    reviewObject.date = date;
+    // console.log(reviewObject)
   }
 }
-
 
 
