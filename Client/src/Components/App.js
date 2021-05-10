5cc3264323ea2d9b5279204a09aaa86f3fada2b8
Jacob Metevia <ja.metevia@gmail.com>
2021-05-10 15:45:22 -0700

    Update to comment out carousel reviews in app

--- Client/src/Components/App.js
+++ Client/src/Components/App.js
@@ -197,23 +197,22 @@ class App extends React.Component {
       }
     })
   }
-  carouselReviewsGetter () {
-    $.ajax({
-      url: "http://localhost:4000/reviews/carouselReviews",
-      data: {ids: [1, 2, 3, 4, 5, 6, 7]},
-      method: 'POST',
-      success: (data) => {
-        this.setState({carouselReviews: data})
-      },
-      error: (error) => {
-        console.log('error', error);
-      }
-    })
-  }
+  // carouselReviewsGetter () {
+  //   $.ajax({
+  //     url: "http://localhost:4000/reviews/carouselReviews",
+  //     data: {ids: [1, 2, 3, 4, 5, 6, 7]},
+  //     method: 'POST',
+  //     success: (data) => {
+  //       this.setState({carouselReviews: data})
+  //     },
+  //     error: (error) => {
+  //       console.log('error', error);
+  //     }c
+  //   })
+  // }
 
   componentDidMount() {
     this.reviewGetter();
-    this.carouselReviewsGetter();
   }
   render() {
     return (
