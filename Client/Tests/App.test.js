d0b40743a6392e3205351a26ddbe1307584f6f2f
Jacob <ja.metevia@gmail.com>
2021-05-12 16:33:38 -0700

    4 tests passing, enzyme not working with jest

--- Client/Tests/App.test.js
+++ Client/Tests/App.test.js
@@ -6,23 +6,40 @@ import App from '../src/Components/App.js';
 
 
 it ('should initialize App with an empty array of reviews', async () => {
-  let mockGetter = jest.fn();
-  App.prototype.reviewGetter = mockGetter;
+  // let mockGetter = jest.fn();
+  // App.prototype.reviewGetter = mockGetter;
   let app = mount(<App />);
   let reviewArray = app.state('reviews');
   expect(reviewArray).toStrictEqual([]);
 })
 
-it ('should have a filters class with 2 children', async () => {
-  let mockGetter = jest.fn();
+it ('should render revierShell if reviewGetter returns null', async () => {
+  let mockGetter = jest.fn(() => {return 'error'});
   App.prototype.reviewGetter = mockGetter;
-  let app = mount(<App />);
+  let app = shallow(<App />)
+  let wait = await mockGetter();
+  expect(app.exists('.reviewsShell').toEqual(true));
+})
+
+it ('should have a filters class with 2 children', async () => {
+  let app = shallow(<App />);
   expect(app.find('.filters').children()).toHaveLength(2);
 })
 
-test('shallow wrapper instance should not be null', () => {
+it('should have a shallow wrapper instance not equal to null', () => {
   const app = shallow(<App />);
   const instance = app.instance();
   expect(instance).toBeInstanceOf(App);
 });
+
+
+it ('should call setCanadaClass', () => {
+  App.prototype.setCanadaClass = jest.fn(() => {return 'canada'});
+  const spy = jest.spyOn(App.prototype, "setCanadaClass");
+  const app = shallow(<App />);
+  app.instance().setCanadaClass();
+  expect(spy).toHaveBeenCalled();
+})
+
+
 
