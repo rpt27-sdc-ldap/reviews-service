4c45811f6f3af796785ad28108198b0ab1d5e783
Jacob <ja.metevia@gmail.com>
2021-05-14 17:37:00 -0700

    added api route tests

--- Client/Tests/IntegrationApp.test.js
+++ Client/Tests/IntegrationApp.test.js
@@ -10,6 +10,14 @@ it ('should fetch a created user', async () => {
     expect(response.statusCode).toBe(200);
 });
 
+it ('should fetch carousel routes as well', async () => {
+  const response = await request.post("/reviews/carouselReviews").send({ids: [1, 2, 3]});
+  expect(response.body.length).toBeGreaterThan(0);
+  expect(response.body[0]).toHaveProperty("bookId");
+  expect(response.body[0]).toHaveProperty("reviewerName");
+  expect(response.statusCode).toBe(200);
+})
+
 
 
 
