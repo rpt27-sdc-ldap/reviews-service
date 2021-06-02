16965241989d4dacb99058c2dd89841d74688b9c
Jacob <ja.metevia@gmail.com>
2021-06-02 16:38:29 -0700

    new changed

--- Client/src/Components/App.js
+++ Client/src/Components/App.js
@@ -168,6 +168,7 @@ class App extends React.Component {
     let pathname = document.location.pathname;
     const query = new URLSearchParams(location.search);
     const bookId = query.get('bookId');
+    console.log(document.location.pathname);
     fetch(`http://ec2-54-153-95-228.us-west-1.compute.amazonaws.com:4000/books/${bookId}/reviews`)
       .then((response) => response.json())
       .then(data => {
